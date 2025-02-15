85c6d6e37ee29dd9db5474e20164469855b70927
pelikhan <pelikhan@users.noreply.github.com>
2025-02-15 16:08:39 +0000

    Deploying to gh-pages from @ microsoft/genaiscript@44fe3e6e154fd5aff79c31e21e58cb93ff23bce2 🚀

---
+++ slides/overview-may2024/assets/slidev/default-CNSqR-Lc.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-CCi0VKWM.js";import{_ as r}from"../index-rG1YBRbR.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
---
+++ slides/pnw-plse-may2024/assets/slidev/default-BHtaO2XE.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-DkSua2Ow.js";import{_ as r}from"../index-DeEH9lyl.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
